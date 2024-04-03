df17335bc643deed3d35985d578fa906f25bbb3d
dige1997 <nicolai.dn@gmail.com>
2024-04-03 13:57:30 +0200

    Update HeroBanner and NavBar components

--- my-app/app/Components/HeroBanner.jsx
+++ my-app/app/Components/HeroBanner.jsx
@@ -21,11 +21,11 @@ export default function HeroBanner() {
 
         <div className="bg-slate-50 z-10 shadow-md">
             <div className="" style={image}>
-            <div className="right-20 top-20 absolute" style={image2}>
+            <div className="right-20 top-20 absolute border-b-slate-700" style={image2}>
             </div>
             <div className="top-48 left-36 absolute">
                 <h1 className="text-7xl font-bold text-white ">Nicolai Dige</h1>
-                <p className="text-5xl text-white">Web Developer</p>
+                <p className="text-3xl text-white">Web Developer Student</p>
                 </div>
             </div>
         </div>
--- my-app/app/Components/NavBar.jsx
+++ my-app/app/Components/NavBar.jsx
@@ -1,11 +1,9 @@
-'use client'
+'use client';
 import { useEffect, useState } from "react";
-
-
-
 
 export default function NavBar() {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleScroll = () => {
         if (window.scrollY > 10) {
@@ -13,6 +11,10 @@ export default function NavBar() {
         } else {
             setIsScrolled(false);
         }
+    };
+
+    const handleMenuToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
     };
 
     useEffect(() => {
@@ -23,22 +25,27 @@ export default function NavBar() {
     }, []);
 
     return (
-        <nav className={`fixed z-50 top-0 w-full p-4 transition-all duration-500 ${isScrolled ? 'bg-gray-500 shadow-md' : 'bg-transparent text-transparent'}`}>
-            <div className="mx-auto flex justify-center items-center">
-                <ul className="flex space-x-32">
+        <nav className={`fixed z-50 ${isScrolled ? 'bg-gray-100 shadow-md rounded-full' : 'bg-transparent'} top-3 right-3 w-50px h-50px align-middle transition-all duration-500`}>
+            <div className="mx-auto flex justify-between items-center">
+                <div>
+                    <button className="text-gray-500 focus:outline-none align-middle p-4" onClick={handleMenuToggle}>
+                        <svg className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"} />
+                        </svg>
+                    </button>
+                </div>
+                <ul className={`flex flex-col rounded-none bg-white absolute top-20 -right-3 ${isMenuOpen ? 'block w-70 h-auto' : 'hidden'} p-4 text-xl`}>
                     <li>
-                        <a href="#" className="font-semibold ">Home</a>
+                        <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-200 font-bold">About me</a>
                     </li>
                     <li>
-                        <a href="#" className="">About</a>
+                        <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-200 font-bold">My projects</a>
                     </li>
                     <li>
-                        <a href="#" className="">Contact</a>
+                        <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-200 font-bold">Contact me</a>
                     </li>
                 </ul>
             </div>
         </nav>
-        
-
     );
 }
